ab252542a3fcc1928e3e8de69a80adc0ddc769d9
WJT94 <williamjt94@gmail.com>
2022-09-26 14:45:20 +0100

    add JavaCore package

---
+++ JavaCore/JavaCore/src/datatype/DatatypeExample.java
@@ -0,0 +1,27 @@
+package datatype;
+
+public class DatatypeExample {
+    public static void main(String[] args) {
+        // All data types
+        // Declararation: datatypename variablename = value;
+        byte a = 2;
+        short b = 20;
+        int c = 23456;
+        long d = 345678l;
+        
+        float x = 12.55f;
+        double y = 645.66;
+
+        char ans = '\u0000';
+        boolean valid = true;
+
+        System.out.println("Byte type: " + a);
+        System.out.println("Short type: " + b);
+        System.out.println("Integer type: " + c);
+        System.out.println("Long type: " + d);
+        System.out.println("Float type: " + x);
+        System.out.println("Double type: " + y);
+        System.out.println("Char type: " + ans);
+        System.out.println("Boolean type: " + valid);
+    }
+}
