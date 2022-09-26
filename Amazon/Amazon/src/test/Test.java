b22c9bf63ebdaaef5d7e15c21a0f621dd01c6c5d
WJT94 <williamjt94@gmail.com>
2022-09-26 17:06:05 +0100

    update amazon

--- Amazon/Amazon/src/test/Test.java
+++ Amazon/Amazon/src/test/Test.java
@@ -1,7 +1,17 @@
 package test;
 
+import customer.*;
+import payment.*;
+import product.*;
+
 public class Test {
     public static void main(String[] args) {
+        Customer customerObj = new Customer();
+        Product productObj = new Product();
+        Payment paymentObj = new Payment();
         
+        customerObj.show();
+        productObj.showProduct();
+        paymentObj.showPayment();
     }
 }
