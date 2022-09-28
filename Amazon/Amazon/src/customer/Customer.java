ff2c6cea82a6e8b548ce03b4758771b46c981875
WJT94 <williamjt94@gmail.com>
2022-09-28 09:38:50 +0100

    update Customer class

--- Amazon/Amazon/src/customer/Customer.java
+++ Amazon/Amazon/src/customer/Customer.java
@@ -1,6 +1,26 @@
 package customer;
 
+import product.Product;
+
 public class Customer {
+    String name;
+    String address;
+    String phone;
+    Product[] cart;
+
+    public static Customer createNew() {
+        Customer cus = new Customer();
+
+
+        return cus;
+    }
+    
+    public void initialize(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
     public void show() {
         System.out.println("Hello, customers!");
     }
