b0b3af9d13c6bafe03066f26141a11da125c0b9e
WJT94 <williamjt94@gmail.com>
2022-10-04 10:53:51 +0100

    update

--- JavaCore/JavaCore/src/inheritance/Person.java
+++ JavaCore/JavaCore/src/inheritance/Person.java
@@ -2,12 +2,10 @@ package inheritance;
 
 public class Person {
     public String name;
-    public int age; // we've now established Person instances should have an "age" variable
+    public int age = 0; // we've now established Person instances should have an "age" variable
 
     public Person(String inputName) { // This one will be called if we only supply one argument, as a string
         this.name = inputName;
-        this.age = 0; // We'll just assume a new Person is a baby :)
-        this.introduce();
     }
 
     public Person(String inputName, int inputAge) { // This one will be called if we supply two arguments, a string and an integer!
--- JavaCore/JavaCore/src/inheritance/Test.java
+++ JavaCore/JavaCore/src/inheritance/Test.java
@@ -2,10 +2,19 @@ package inheritance;
 
 public class Test {
     public static void main(String[] args) {
-        Person person = new Person("Bobby", 14);
 
-        agePerson(person);
-        agePerson(person);
+        Manager man = new Manager("Overlord");
+        Employee emp = (Employee) man;
+        Employee emp2 = new Employee("Second Peon");
+        Employee man2 = new Manager("Second Overlord");
+        
+        emp.talk();
+        man.talk();
+        emp2.talk();
+        man2.talk();
+
+        final int finalInt = 0;
+        System.out.println(finalInt);
     }
 
     public static void agePerson(Person person) {
