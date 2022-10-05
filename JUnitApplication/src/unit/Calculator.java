606fce101f04bd6a0b4c93779db125105124c525
WJT94 <williamjt94@gmail.com>
2022-10-05 09:48:13 +0100

    update wednesday

--- JUnitApplication/src/unit/Calculator.java
+++ JUnitApplication/src/unit/Calculator.java
@@ -16,4 +16,17 @@ public class Calculator {
             return y;
         }
 	}
+
+    // Takes any amount of integers and returns the largest
+    public static int findBiggest(int... nums) {
+        int biggest = nums[0];
+        // System.out.println(String.format("Starting with %s", biggest));
+        for (int i = 1; i < nums.length; i++) {
+            // System.out.println(String.format("Comparing %s to %s", biggest, nums[i]));
+            if (nums[i] > biggest) {
+                biggest = nums[i];
+            }
+        }
+        return biggest;
+    }
 }
